19c28f0f8f4b34638d6ae596823512a98944b8b8
Batuta <batuta2604@gmail.com>
2017-02-16 12:04:37 -0200

    Icones e componetes na tela de cadastro

--- app/src/main/java/br/com/fiap/app/CadastroActivity.java
+++ app/src/main/java/br/com/fiap/app/CadastroActivity.java
@@ -2,6 +2,10 @@ package br.com.fiap.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import static br.com.fiap.app.R.id.edtEmail;
+import static br.com.fiap.app.R.id.edtNome;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -9,6 +13,10 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        //edtNome = (EditText) findViewById(R.id.edtNome);
+        //edtEmail = (EditText) findViewById(R.id.edtEmail);
+
     }//OnCreate
 
 }//Activit
